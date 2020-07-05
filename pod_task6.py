62c00f46b4488d73f8e8f5e2fca365094040039e
Manglam <64706997+Mangalofficial@users.noreply.github.com>
2020-07-06 03:17:25 +0530

    final

--- pod_task6.py
+++
@@ -1,7 +0,0 @@
-open_file=open('/var/lib/jenkins/workspace/JOB2_task6/pod.py','r')
-read=open_file.read()
-for a in read:
-    if a.isupper() == True:
-        pass
-    else:
-       print(a,end='')
